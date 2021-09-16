624866ffb238f9b77c469d7e076794113fdfbd69
Breno Farias <brenofarias.sousa@gmail.com>
2021-09-16 12:59:57 -0300

    ensure LocalLoadPurchases returns a list of purchases if cache is more than 3 days old

--- src/data/usecases/load-purchases/local-load-purchases.spec.ts
+++ src/data/usecases/load-purchases/local-load-purchases.spec.ts
@@ -23,6 +23,7 @@ describe("LocalLoadPurchases", () => {
     const { cacheStore, sut } = makeSut();
     cacheStore.simulateFetchError();
     const purchases = await sut.loadAll();
+
     expect(cacheStore.actions).toEqual([
       CacheStoreSpy.Action.fetch,
       CacheStoreSpy.Action.delete,
@@ -32,15 +33,44 @@ describe("LocalLoadPurchases", () => {
   });
 
   test("Should return a list of purchases if cache is less than 3 days old", async () => {
-    const timestamp = new Date();
-    const { cacheStore, sut } = makeSut(timestamp);
+    const currentDate = new Date();
+    const timestamp = new Date(currentDate);
+
+    timestamp.setDate(timestamp.getDate() - 3);
+    timestamp.setSeconds(timestamp.getSeconds() + 1);
+
+    const { cacheStore, sut } = makeSut(currentDate);
     cacheStore.fetchResult = {
       timestamp,
       value: mockPurchases(),
     };
     const purchases = await sut.loadAll();
+
     expect(cacheStore.actions).toEqual([CacheStoreSpy.Action.fetch]);
     expect(cacheStore.fetchKey).toBe("purchases");
     expect(purchases).toEqual(cacheStore.fetchResult.value);
+  });
+
+  test("Should return an empty list if cache is more than 3 days old", async () => {
+    const currentDate = new Date();
+    const timestamp = new Date(currentDate);
+
+    timestamp.setDate(timestamp.getDate() - 3);
+    timestamp.setSeconds(timestamp.getSeconds() - 1);
+
+    const { cacheStore, sut } = makeSut(currentDate);
+    cacheStore.fetchResult = {
+      timestamp,
+      value: mockPurchases(),
+    };
+    const purchases = await sut.loadAll();
+
+    expect(cacheStore.actions).toEqual([
+      CacheStoreSpy.Action.fetch,
+      CacheStoreSpy.Action.delete,
+    ]);
+    expect(cacheStore.fetchKey).toBe("purchases");
+    expect(cacheStore.deleteKey).toBe("purchases");
+    expect(purchases).toEqual([]);
   });
 });
